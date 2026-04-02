25ba73d9d4a6b7b5743190ced85977ab9bfecfb8
Swati singh <swati.singh.cs24@ggits.net>
2026-04-02 10:47:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(
+    r"C:\Users\Sumit\Desktop\opencv-python\video Proccessing\Face and object detection\haarcascade_frontalface_default.xml"
+)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Webcam Face", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
